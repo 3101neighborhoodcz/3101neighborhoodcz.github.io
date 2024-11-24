b217d95f350bcc178feaaa2080bb0abea28fc24d
pumpstich <peterjamestown2022@gmail.com>
2024-11-23 22:39:29 -0500

    fixing pt3

---
+++ js/news.js
@@ -0,0 +1,8 @@
+// Add event listeners to all divs with the 'news' class
+document.querySelectorAll('.news_article').forEach(div => {
+    div.addEventListener('click', () => {
+        const id = div.id; // Get the ID of the clicked div
+        const url = `/pages/news?article=${id}`; // Construct the URL
+        window.location.href = url; // Redirect to the constructed URL
+    });
+});
