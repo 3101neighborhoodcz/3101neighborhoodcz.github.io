02ce781bdfb6a5616ca44ba832385a960bf31508
pumpstich <peterjamestown2022@gmail.com>
2024-11-24 12:10:21 -0500

    fixing pt3.61

--- js/news.js
+++ js/news.js
@@ -2,7 +2,7 @@
 document.querySelectorAll('.news_article').forEach(div => {
     div.addEventListener('click', () => {
         const id = div.id; // Get the ID of the clicked div
-        const url = `/pages/news?article=${id}`; // Construct the URL
+        const url = `/pages/news/article?article=${id}`; // Construct the URL
         window.location.href = url; // Redirect to the constructed URL
     });
 });
